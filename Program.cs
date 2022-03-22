ee8b7f26aebb0a840930f7a77dcbe6374a72023d
Navneet Thukrul <navneetthukrul2000@gmail.com>
2022-03-22 09:48:12 +0530

    Name changed

--- Program.cs
+++ Program.cs
@@ -2,21 +2,7 @@
 
 using Day14Demo.WaiTech;
 
-
-var resumeForm = new ResumeForm("Ayushi  Tiwari WorldLineResume Application Form ", 10, 4);
-=======
-var resumeForm = new ResumeForm("WorldLineResume Application Form Cherubin Chatta", 10, 4);
-=======
-var resumeForm = new ResumeForm("Divyanshu Dubey - WorldLineResume Application Form", 10, 4);
-=======
-var resumeForm = new ResumeForm("Madhu M S - WorldLineResume Application Form", 10, 4);
-======
-var resumeForm = new ResumeForm("Vasu Yadav - WorldLineResume Application Form", 10,20);
-======
-var resumeForm = new ResumeForm("Monika Abhang - WorldLineResume Application Form", 10,5);
-
-
-
+var resumeForm = new ResumeForm("Navneet - WorldLineResume Application Form", 10, 5);
 
 resumeForm.Show();
 resumeForm.StartInput();
